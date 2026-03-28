3e2f494cb63e5d9f32bc49d2cba99f702222132b
Foysal <foysaljoarder012@gmail.com>
2026-03-28 22:24:07 +0600

    working on Intro Screen 2

---
+++ lib/view/screen/login_introduction/login_introduction_screen2.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:slider_button/slider_button.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class LoginIntroductionScreen2 extends StatelessWidget {
+  LoginIntroductionScreen2({super.key});
+
+  final controller = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+          children: [
+            Container(
+              height: MediaQuery.sizeOf(context).height,
+              width: MediaQuery.sizeOf(context).width,
+              padding: EdgeInsets.all(15.0),
+              decoration: BoxDecoration(
+                // color: Color(0xff504158)
+                  image: DecorationImage(image: AssetImage('assets/image/intro_screen.jpg'), fit: BoxFit.fill)
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Gap(50.0),
+
+                    Text('Start your\nfitness Journey!',
+                      style: TextStyle(
+                          fontSize: 22,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline
+                      ),
+                    ),
+                    Gap(10.0),
+
+                    Text('Start your fitness journey\nwith our app\'s guidance and support',
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white54,
+                            fontWeight: FontWeight.w500,
+                            decoration: TextDecoration.underline
+                        )
+                    ),
+                    Gap(400.0),
+
+                    SliderButton(
+                      action: () async {
+                        // return true; //return false if you want to avoid dismissing the widget in the tree.
+                      },
+                      label: Text(
+                        "Lets start",
+                        style: TextStyle(
+                            color: Color(0xff4a4a4a),
+                            fontWeight: FontWeight.w500,
+                            fontSize: 17),
+                      ),
+                      icon: Center(
+                          child: Icon(
+                            Icons.arrow_forward_ios_rounded,
+                            color: Colors.black,
+                            size: 30.0,
+                            semanticLabel: 'Text to announce in accessibility modes',
+                          )),
+                      width: 400,
+                      radius: 50,
+                      buttonColor: Color(0xffffffff),
+                      backgroundColor: Color(0xff0000D5),
+                      highlightedColor: Colors.white,
+                      baseColor: Colors.red,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            Align(
+              alignment: AlignmentGeometry.xy(50, 50),
+              child: SmoothPageIndicator(
+              controller: controller,
+              count: 4,
+              onDotClicked: (index) {
+                controller.jumpToPage(index);
+              },
+              effect: ColorTransitionEffect(dotColor: Colors.purple,
+                  activeDotColor: Colors.white,
+                  activeStrokeWidth: 15),
+            ),)
+          ],
+        )
+    );
+  }
+}
